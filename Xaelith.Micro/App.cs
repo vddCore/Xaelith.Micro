a148beb1790405a4103fe3d7e17e61d051396c31
vddCore <vddcore@outlook.com>
2025-04-25 14:15:30 +0200

    initial commit.

---
+++ Xaelith.Micro/App.cs
@@ -0,0 +1,30 @@
+namespace Xaelith.Micro;
+
+public partial class App
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services
+            .AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        var app = builder.Build();
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
+    }
+}
