7ff392a603e560d7c12ef13b1b855708c2ad9be0
vddCore <vddcore@outlook.com>
2025-04-30 09:00:35 +0200

    improve title behavior, abstract it away.

---
+++ Xaelith.Micro/Visual/Pages/Base/XaelithPage.razor.cs
@@ -0,0 +1,39 @@
+﻿namespace Xaelith.Micro.Visual.Pages.Base;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Xaelith.Micro.Infrastructure.ServiceModel.Core;
+using Xaelith.Micro.Infrastructure.ServiceModel.Core.Content;
+
+public partial class XaelithPage : ComponentBase
+{
+    [Inject] protected IJSRuntime JS { get; private set; } = null!;
+    [Inject] protected IConfigService Configuration { get; private set; } = null!;
+    [Inject] protected IContentService Content { get; private set; } = null!;
+    [Inject] protected IMarkdownService Markdown { get; private set; } = null!;
+
+    protected virtual string Title => string.Empty;
+
+    protected virtual async Task UpdateTitleAsync()
+    {
+        var title = $"{Configuration.Root!.General.SiteTitle}";
+
+        if (!string.IsNullOrWhiteSpace(Title))
+        {
+            title += " // " + Title;
+        }
+
+        await JS.InvokeVoidAsync(
+            "window.xaelith.setDocumentTitle",
+            title
+        );
+    }
+    
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await UpdateTitleAsync();
+        }
+    }
+}
--- Xaelith.Micro/wwwroot/lib/init/init.js
+++ Xaelith.Micro/wwwroot/lib/init/init.js
@@ -9,42 +9,14 @@
     highlightAll: highlightAll
   };
   
-  window.xaelith.filterHeaderAnimation = function() {
-    var h1 = document
-      .getElementsByClassName("filter-header")[0]
-      .getElementsByTagName("h1")[0];
-                 
-    var h2 = document
-      .getElementsByClassName("filter-header")[0]
-      .getElementsByTagName("h2")[0];
-                     
-    if (h1) {
-      window.setTimeout(function() {
-        h1.style.width = "100%";
-      }, 75);
-      
-      window.setTimeout(function() {
-        h1.style.overflow = "auto";
-        h1.style.textWrap = "wrap";
-      }, 1000);
-    }
-
-    if (h2) {
-      window.setTimeout(function() {
-        h2.style.width = "100%";
-      }, 150);
-      
-      window.setTimeout(function() {
-        h2.style.overflow = "auto";
-        h2.style.textWrap = "wrap";
-      }, 1000);
-    }
-  }
+  window.xaelith.setDocumentTitle = (title) => {
+      document.title = title;
+  };
 })();
 
 let prevScrollpos = window.pageYOffset;
 
-window.onscroll = function() {
+window.onscroll = () => {
   if (window.innerWidth <= 736) {
     let currentScrollPos = window.pageYOffset;
 
