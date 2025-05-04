ad39b4b410a36815e969cd1a4d1987e17c4c7449
vddCore <vddcore@outlook.com>
2025-05-05 00:23:17 +0200

    add tag editor.

---
+++ Xaelith.Micro/Infrastructure/DataModel/Admin/DualValueEditContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Xaelith.Micro.Infrastructure.DataModel.Admin;
+
+public class DualValueEditContext
+{
+    public record Entry(string firstValue, string secondValue)
+    {
+        public string FirstValue { get; set; } = firstValue;
+        public string SecondValue { get; set; } = secondValue;
+    }
+
+    public List<Entry> Entries { get; set; } = [];
+}
