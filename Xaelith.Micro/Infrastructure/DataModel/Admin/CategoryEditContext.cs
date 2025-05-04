37b175ada4d809d4dd746cbfb33fcddc2ff8e597
vddCore <vddcore@outlook.com>
2025-05-04 23:23:50 +0200

    add category editor.

---
+++ Xaelith.Micro/Infrastructure/DataModel/Admin/CategoryEditContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Xaelith.Micro.Infrastructure.DataModel.Admin;
+
+public class CategoryEditContext
+{
+    public class Category
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+    
+    public List<Category> Categories { get; set; } = [];
+}
