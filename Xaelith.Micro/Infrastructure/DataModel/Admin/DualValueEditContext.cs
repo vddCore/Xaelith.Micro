35e00b7d03b6c64fb864c6fe7c15b319eb2c431a
vddCore <vddcore@outlook.com>
2025-05-05 23:11:00 +0200

    add prototype for nav editor.

--- Xaelith.Micro/Infrastructure/DataModel/Admin/DualValueEditContext.cs
+++ Xaelith.Micro/Infrastructure/DataModel/Admin/DualValueEditContext.cs
@@ -2,10 +2,16 @@
 
 public class DualValueEditContext
 {
-    public record Entry(string firstValue, string secondValue)
+    public record Entry
     {
-        public string FirstValue { get; set; } = firstValue;
-        public string SecondValue { get; set; } = secondValue;
+        public string FirstValue { get; set; }
+        public string SecondValue { get; set; }
+        
+        public Entry(string firstValue, string secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+        }
     }
 
     public List<Entry> Entries { get; set; } = [];
---
+++ Xaelith.Micro/Infrastructure/DataModel/Admin/NavGroupEditContext.cs
@@ -0,0 +1,7 @@
+﻿namespace Xaelith.Micro.Infrastructure.DataModel.Admin;
+
+public record NavGroupEditContext
+{
+    public required string Name { get; set; }
+    public required DualValueEditContext Links { get; set; }
+}
