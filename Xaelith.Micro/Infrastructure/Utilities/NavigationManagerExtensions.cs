c72d653bb30246b67858844f99416394195b7e62
vddCore <vddcore@outlook.com>
2025-05-09 09:28:10 +0200

    fix some minor annoyances.

--- Xaelith.Micro/Infrastructure/Utilities/NavigationManagerExtensions.cs
+++ Xaelith.Micro/Infrastructure/Utilities/NavigationManagerExtensions.cs
@@ -3,6 +3,7 @@
 using System.Collections.Specialized;
 using System.Web;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
 
 public static class NavigationManagerExtensions
 {
@@ -12,4 +13,18 @@ public static class NavigationManagerExtensions
     public static string QueryString(this NavigationManager navigationManager, string key)
         => navigationManager.QueryString()[key] 
         ?? string.Empty;
+    
+    public static string GetUriWithQueryParameter(this NavigationManager navigationManager, string key, object? value)
+    {
+        var uri = navigationManager.ToAbsoluteUri(navigationManager.Uri);
+        var baseUri = uri.GetLeftPart(UriPartial.Path);
+        var query = QueryHelpers.ParseQuery(uri.Query);
+        var dict = query.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.ToString());
+
+        dict[key] = value?.ToString() ?? string.Empty;
+
+        var newQuery = QueryHelpers.AddQueryString(baseUri, dict!);
+
+        return newQuery;
+    }
 }
